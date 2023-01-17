34c2a9d2f724a9a397ecbefe7dd0ca891d3b8e1e
mrblade <bullet_harsimran@yahoo.com>
2023-01-17 16:18:42 +0530

    Fix

--- code/scrape.py
+++ code/scrape.py
@@ -12,9 +12,8 @@ for el in content:
     time = el.select('.news-card-author-time-in-title .time')[0].text
     url = el.select('.read-more a')[0]['href'] if el.select('.read-more a') else "https://www.inshorts.com" + el.select('.news-card-title .clickable')[0]['href']
     res.append({'title': title.strip(), "url" : url.strip(), "time" : time.strip() })
-#if not os.path.isdir(mypath):
-   #os.makedirs(mypath)
+
 mode = 'a' if os.path.exists(mypath) else 'w'
-with open(mypath + '/inshorts.json', mode) as f:
+with open('dump.json', mode) as f:
     json.dump(res, f)
     print('Inshorts updated')
