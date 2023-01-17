a3bc618d85bdfc69b0dd423d0a272952fc29bcfa
Harsimran Singh <bullet_harsimran@yahoo.com>
2023-01-17 17:48:34 +0530

    Update scrape.py

--- code/scrape.py
+++ code/scrape.py
@@ -1,18 +1,16 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-import os
 r = requests.get('https://www.inshorts.com/en/read/')
 res = []
 soup = BeautifulSoup(r.content, 'html.parser')
 content = soup.find_all('div', class_='news-card')
-mypath = os.path.abspath(os.curdir)
 for el in content:
     title = el.select('.news-card-title .clickable span')[0].text
     time = el.select('.news-card-author-time-in-title .time')[0].text
     url = el.select('.read-more a')[0]['href'] if el.select('.read-more a') else "https://www.inshorts.com" + el.select('.news-card-title .clickable')[0]['href']
     res.append({'title': title.strip(), "url" : url.strip(), "time" : time.strip() })
 
-with open(mypath + "/inshorts.json", 'w') as f:
+with open("inshorts.json", 'w') as f:
     json.dump(res, f)
     print('Inshorts updated')
